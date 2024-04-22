8c948195a7d2d426c2aac242b01cd46f26e8cecc
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-04-22 19:58:30 -0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBUxAyiAfCUSoEQBhHnm39yvEsZeGf-eYA",
+    authDomain: "our-place-jumbaegab.firebaseapp.com",
+    projectId: "our-place-jumbaegab",
+    storageBucket: "our-place-jumbaegab.appspot.com",
+    messagingSenderId: "193782402040",
+    appId: "1:193782402040:web:57e7692638165be2c3a40d"
+  }
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app'
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -11,10 +13,23 @@ if (environment.production) {
   enableProdMode();
 }
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBUxAyiAfCUSoEQBhHnm39yvEsZeGf-eYA",
+  authDomain: "our-place-jumbaegab.firebaseapp.com",
+  projectId: "our-place-jumbaegab",
+  storageBucket: "our-place-jumbaegab.appspot.com",
+  messagingSenderId: "193782402040",
+  appId: "1:193782402040:web:57e7692638165be2c3a40d"
+}
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideFirestore(() => getFirestore())
+    ])
   ],
 });
