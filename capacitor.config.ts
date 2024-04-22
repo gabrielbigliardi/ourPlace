c0e5a28d9afcccdf8e56084bc5237c20126bca8f
Gabriel Bigliardi <gabrielbigliardi@gmail.com>
2024-04-22 15:09:14 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'our-place',
+  webDir: 'www'
+};
+
+export default config;
